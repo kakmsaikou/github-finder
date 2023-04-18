dd34b0e8ae1193650cc85689961745ec0b4f0823
TacitusPie <kakmsaikou@gmail.com>
2023-04-18 10:20:47 +0800

    feat: 创建 Home、About、404 路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/layout/Footer';
+import NotFound from './pages/NotFound';
+import About from './pages/About';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <Router>
       <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className='container mx-auto px-3 pb-12'>Content</main>
+        <Navbar title='Hello World' />
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/home' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
