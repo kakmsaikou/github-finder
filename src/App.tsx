1efe6d5f739848da52193a8502f5e630fb77e19e
TacitusPie <kakmsaikou@gmail.com>
2023-04-18 09:46:18 +0800

    feat: 完成 Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
 const App = () => {
-  return <button className="btn">Hello daisyUI</button>
+  return (
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main>Content</main>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
