3e3c119e5537c719077d73776c419508e6a32d9c
TacitusPie <kakmsaikou@gmail.com>
2023-04-20 09:02:26 +0800

    feat: 创建 UserItem

--- src/components/users/UserResults.tsx
+++ src/components/users/UserResults.tsx
@@ -1,9 +1,5 @@
 import { useEffect, useState } from 'react';
-
-interface User {
-  login: string;
-  id: number;
-}
+import UserItem from './UserItem';
 
 const UserResults = () => {
   const [users, setUsers] = useState([] as User[]);
@@ -15,12 +11,12 @@ const UserResults = () => {
 
   const fetchUsers = async () => {
     const response = await fetch(
-      `${import.meta.env.VITE_REACT_APP_GITHUB_URL}/users`,
-      {
-        headers: {
-          Authorization: `token ${import.meta.env.VITE_REACT_APP_GITHUB_TOKEN}`,
-        },
-      }
+      `${import.meta.env.VITE_REACT_APP_GITHUB_URL}/users`
+      // {
+      //   headers: {
+      //     Authorization: `token ${import.meta.env.VITE_REACT_APP_GITHUB_TOKEN}`,
+      //   },
+      // }
     );
     const data = await response.json();
 
@@ -28,10 +24,13 @@ const UserResults = () => {
     setLoading(false);
   };
 
-  return loading ? <h3>Loading...</h3>: (
+  return loading ? (
+    <h3>Loading...</h3>
+  ) : (
     <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
       {users.map(user => (
-        <h3 key={user.id}>{user.login}</h3>
+        <UserItem key={user.id} user={user}/>
+        // <h3 key={user.id}>{user.login}</h3>
       ))}
     </div>
   );
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+interface User {
+  id: number;
+  login: string;
+  avatar_url: string;
+}
