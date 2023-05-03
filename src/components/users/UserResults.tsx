1cd2f236caa7d52fc9694b7eae0d53d06c519dca
TacitusPie <kakmsaikou@gmail.com>
2023-05-03 20:18:46 +0800

    chore: 删除 searchUsers

--- src/components/users/UserResults.tsx
+++ src/components/users/UserResults.tsx
@@ -1,17 +1,13 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import UserItem from './UserItem';
 import { GithubContext } from '../../context/github/GithubContext';
 import Spinner from '../layout/Spinner';
 
 const UserResults = () => {
-  const { users, loading, searchUsers: fetchUsers } = useContext(GithubContext);
-
-  useEffect(() => {
-    // fetchUsers();
-  }, []);
+  const { users, loading } = useContext(GithubContext);
 
   return loading ? (
-    <Spinner/>
+    <Spinner />
   ) : (
     <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
       {users.map(user => (
